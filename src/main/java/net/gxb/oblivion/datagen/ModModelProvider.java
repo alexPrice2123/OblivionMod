a9eba9b3defca4f87fcdb0128d195b90a7d2842f
alexPrice2123 <alexander.price2123@gmail.com>
2026-07-09 13:43:00 -0600

    new build lol

--- src/main/java/net/gxb/oblivion/datagen/ModModelProvider.java
+++ src/main/java/net/gxb/oblivion/datagen/ModModelProvider.java
@@ -15,6 +15,29 @@ public class ModModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
         basicItem(ModItems.EXTRACTION_CORE.get());
+        basicItem(ModItems.ESSENCE_CORE.get());
+        basicItem(ModItems.ESSENCE.get());
+
         basicItem(ModItems.LIGHTNING_ENERGY.get());
+        basicItem(ModItems.LIGHTNING_ESSENCE.get());
+        basicItem(ModItems.VOID_ENERGY.get());
+        basicItem(ModItems.VOID_ESSENCE.get());
+        basicItem(ModItems.PURE_ENERGY.get());
+        basicItem(ModItems.PURE_ESSENCE.get());
+        basicItem(ModItems.CORRUPT_ENERGY.get());
+        basicItem(ModItems.CORRUPT_ESSENCE.get());
+
+        basicItem(ModItems.ABYSSAL_ENERGY.get());
+        basicItem(ModItems.ABYSSAL_ESSENCE.get());
+        basicItem(ModItems.DIVINE_ENERGY.get());
+        basicItem(ModItems.DIVINE_ESSENCE.get());
+        basicItem(ModItems.DUALITY_ENERGY.get());
+        basicItem(ModItems.DUALITY_ESSENCE.get());
+        basicItem(ModItems.ENTROPY_ENERGY.get());
+        basicItem(ModItems.ENTROPY_ESSENCE.get());
+        basicItem(ModItems.WRAITH_ENERGY.get());
+        basicItem(ModItems.WRAITH_ESSENCE.get());
+        basicItem(ModItems.ASTRAL_ENERGY.get());
+        basicItem(ModItems.ASTRAL_ESSENCE.get());
     }
 }
